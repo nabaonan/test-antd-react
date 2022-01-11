1045a3d1547d51b1173df9033ec35a8aee2db250
nbn <nbn1209@126.com>
2022-01-11 23:56:06 +0800

    feat: [编辑弹框]

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Fragment } from "react";
-import { Col, DatePicker } from "antd";
+import { Fragment, useRef } from "react";
+import { Col, Button } from "antd";
 import { Test } from "@components/test";
+import DrawerForm from './components/DrawerForm'
 const App = () => {
+
+  const drawer = useRef<{
+    show: (data?: Record<string, string>) => void
+  }>();
+  const add = () => {
+    console.log('添加')
+    if (drawer && drawer.current) {
+      drawer.current.show()
+
+    }
+  }
+  const edit = () => {
+    drawer.current.show({
+      type: '123123',
+      owner: '33333'
+    })
+  }
+
   return (
     <Fragment>
       <div
@@ -14,10 +33,14 @@ const App = () => {
       >
         <div>
           <h1>Hello AsurRaa</h1>
-          <DatePicker />
+          <Button onClick={add}>添加</Button>
+          <Button onClick={edit}>编辑</Button>
           <Col></Col>
         </div>
+
+
       </div>
+      <DrawerForm ref={drawer} ></DrawerForm>
     </Fragment>
   );
 };
---
+++ src/components/DrawerForm.tsx
@@ -0,0 +1,164 @@
+import { Drawer, Form, Button, Col, Row, Input, Select, DatePicker, Space } from 'antd';
+import { forwardRef, memo, useImperativeHandle, useState } from 'react';
+
+
+const { Option } = Select;
+
+function DrawerForm(props: any, ref: any) {
+
+
+  const [visible, setVisible] = useState(false)
+
+  const [title, setTitle] = useState('')
+  const showDrawer = () => {
+    setVisible(true)
+  };
+
+  const onClose = () => {
+    setVisible(false)
+
+
+  };
+
+  const [form] = Form.useForm();
+
+  useImperativeHandle(ref, () => ({
+    show(data: any) {
+      setVisible(true);
+
+      if (data) {
+        //修改编辑部分
+        console.log('这是啥', data, form)
+        setTitle('编辑')
+        /** code to do ... */
+        form.setFieldsValue(data);
+      } else {
+        form.resetFields()
+        setTitle('添加')
+      }
+    }
+  }));
+
+  return (
+    <>
+      <Button type="primary" onClick={showDrawer} >
+        New account
+      </Button>
+      <Drawer
+        title={title}
+        width={720}
+        onClose={onClose}
+        visible={visible}
+        bodyStyle={{ paddingBottom: 80 }}
+        extra={
+          <Space>
+            <Button onClick={onClose}>Cancel</Button>
+            <Button onClick={onClose} type="primary">
+              Submit
+            </Button>
+          </Space>
+        }
+      >
+        <Form layout="vertical" form={form} hideRequiredMark>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="name"
+                label="Name"
+                rules={[{ required: true, message: 'Please enter user name' }]}
+              >
+                <Input placeholder="Please enter user name" />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="url"
+                label="Url"
+                rules={[{ required: true, message: 'Please enter url' }]}
+              >
+                <Input
+                  style={{ width: '100%' }}
+                  addonBefore="http://"
+                  addonAfter=".com"
+                  placeholder="Please enter url"
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="owner"
+                label="Owner"
+                rules={[{ required: true, message: 'Please select an owner' }]}
+              >
+                <Select placeholder="Please select an owner">
+                  <Option value="xiao">Xiaoxiao Fu</Option>
+                  <Option value="mao">Maomao Zhou</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="type"
+                label="Type"
+                rules={[{ required: true, message: 'Please choose the type' }]}
+              >
+                <Select placeholder="Please choose the type">
+                  <Option value="private">Private</Option>
+                  <Option value="public">Public</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="approver"
+                label="Approver"
+                rules={[{ required: true, message: 'Please choose the approver' }]}
+              >
+                <Select placeholder="Please choose the approver">
+                  <Option value="jack">Jack Ma</Option>
+                  <Option value="tom">Tom Liu</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="dateTime"
+                label="DateTime"
+                rules={[{ required: true, message: 'Please choose the dateTime' }]}
+              >
+                <DatePicker.RangePicker
+                  style={{ width: '100%' }}
+                  getPopupContainer={trigger => trigger.parentElement}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={24}>
+              <Form.Item
+                name="description"
+                label="Description"
+                rules={[
+                  {
+                    required: true,
+                    message: 'please enter url description',
+                  },
+                ]}
+              >
+                <Input.TextArea rows={4} placeholder="please enter url description" />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Drawer>
+    </>
+  )
+}
+
+
+
+export default memo(forwardRef(DrawerForm));
