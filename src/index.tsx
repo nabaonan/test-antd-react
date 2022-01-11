9f6295e7bf74a45556753c98499c49ebbb4e4c39
内小子 <897250040@qq.com>
2022-01-11 23:18:19 +0800

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "antd/dist/antd.less";
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
